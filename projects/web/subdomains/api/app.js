9fa059fed1d97305a9438c46fa9d94abcc7ab553
Steve Johnson <steve@rewardme.com>
2012-11-27 18:31:20 +0700

    start working on server - add express and basic app.js file. using supervisor for managing node instances;

--- projects/web/subdomains/api/app.js
+++ projects/web/subdomains/api/app.js
@@ -1,1 +1,41 @@
+/***************** Dependencies and Initialization *****************/
+var express = require('express');
+var app = express();
 
+
+
+
+/***************** Logging *****************/
+app.all('*', function(req, res, next){
+  console.log(req.ip + " " + req.path);
+  next();
+});
+
+/***************** Bad Request Handling *****************/
+app.all('/', function(req, res){
+	res.redirect('http://www.LightSwarm.com');
+});
+
+
+
+
+/***************** Request Handling *****************/
+app.all('/:objType/:action/:id', function(req, res){
+  res.send('Hello World - ' + req.params.objType + "." + req.params.action + "." + req.params.id + "\n");
+});
+
+
+
+
+
+
+/***************** Socket Bind *****************/
+app.enable('trust proxy')
+app.listen(3000);
+console.log('Listening on port 3000');
+
+/***************** Error Handling *****************/
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
+});
